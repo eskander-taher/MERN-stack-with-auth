9698ab99056bb55b8d909a2d521f5b18144807c2
eskander-taher <eskander.alshaibany@gmail.com>
2022-08-15 02:17:20 +0500

    finish goal api with CRUD functionalty

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const color = require('colors')
 const cors = require('cors')
+const dotenv = require('dotenv').config()
+
 const goalRoutes = require('./routes/goalRoutes')
 const {errorHandler} = require('./middleware/errorMiddleware')
-const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+
+connectDB()
+
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -10,6 +16,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
 
 app.use('/api/goals', goalRoutes)
 app.use(errorHandler)
